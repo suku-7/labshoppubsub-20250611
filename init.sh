4eed28d1c530a7370459849a9094212eabd02c68
acmexii <apexacme@gmail.com>
2023-04-28 05:11:08 +0000

    Updated

--- init.sh
+++ init.sh
@@ -1,14 +1,16 @@
 sudo apt-get update
 sudo apt-get install net-tools
 sudo apt install iputils-ping
+
+pip install httpie
+
 wget https://download.joedog.org/siege/siege-4.0.4.tar.gz
 tar -xvf siege-4.0.4.tar.gz
 cd siege-4.0.4
 ./configure --prefix=$HOME/siege > /dev/null 2>&1
 sudo make install > /dev/null 2>&1
-export PATH=$PATH:$HOME/siege/bin
-cd /workspace/lab-shop-monolith/
-pip install httpie
+echo 'export PATH=$PATH:$HOME/siege/bin' >> ~/.bashrc
+cd /workspace/lab-shop-pub-sub
 
 cd kafka
 docker-compose up
