ee3392be3d8a37682055670ba9cddaa7bad9b631
acmexii <apexacme@gmail.com>
2023-04-28 04:57:12 +0000

    updated - Siege 404 install

--- init.sh
+++ init.sh
@@ -1,7 +1,13 @@
 sudo apt-get update
 sudo apt-get install net-tools
 sudo apt install iputils-ping
-sudo apt install siege
+wget https://download.joedog.org/siege/siege-4.0.4.tar.gz
+tar -xvf siege-4.0.4.tar.gz
+cd siege-4.0.4
+./configure --prefix=$HOME/siege > /dev/null 2>&1
+sudo make install > /dev/null 2>&1
+export PATH=$PATH:$HOME/siege/bin
+cd /workspace/lab-shop-monolith/
 pip install httpie
 
 cd kafka
