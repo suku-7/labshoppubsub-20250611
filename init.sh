be032675488a1be7ba97cc8bf97f500ceb11f675
acmexii <apexacme@gmail.com>
2023-08-30 10:48:21 +0000

    init script modified

--- init.sh
+++ init.sh
@@ -4,13 +4,13 @@ sudo apt install iputils-ping
 
 pip install httpie
 
-wget https://download.joedog.org/siege/siege-4.0.4.tar.gz
-tar -xvf siege-4.0.4.tar.gz
-cd siege-4.0.4
-./configure --prefix=$HOME/siege > /dev/null 2>&1
-sudo make install > /dev/null 2>&1
-echo 'export PATH=$PATH:$HOME/siege/bin' >> ~/.bashrc
-cd /workspace/lab-shop-pub-sub
+wget https://download.joedog.org/siege/siege-4.0.4.tar.gz &&
+tar -xvf siege-4.0.4.tar.gz &&
+cd siege-4.0.4 &&
+./configure --prefix=$HOME/siege > /dev/null 2>&1 &&
+sudo make install > /dev/null 2>&1 &&
+echo 'export PATH=$PATH:$HOME/siege/bin' >> ~/.bashrc &&
+cd /workspace/lab-shop-pub-sub &&
 
 cd kafka
 docker-compose up
